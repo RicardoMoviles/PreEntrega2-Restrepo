395c6085b9f8a5eebbcc456b13ccb4aa35d952de
RicardoRpo <ricar2011.medallo@gmail.com>
2024-02-11 20:33:06 -0500

    funcion buscarMejorTasa finalizada imprime un mensaje sugiriendo el tiempo a invertir

--- js/index.js
+++ js/index.js
@@ -2,15 +2,16 @@ let montoDeInversion;
 let tiempoDeInversion;
 let interesEfectivoAnual;
 let mejorTasa;
+let diasTasaMasAlta;
 let tablaTasas = [
-    [9.4, 9.5, 10.4, 10.5, 10.6, 10.75, 10,65, 10.25, 9.5, 9.2, 9.2, 9.2],
-    [9.4, 9.5, 10.4, 10.5, 10.6, 10.75, 10,65, 10.25, 9.6, 9.3, 9.3, 9.3],
-    [9.5, 9.6, 10.5, 10.6, 10.7, 10.85, 10,75, 10.35, 9.7, 9.4, 9.4, 9.4],
-    [9.6, 9.7, 10.6, 10.65, 10.75, 10.9, 10,8, 10.45, 9.8, 9.5, 9.5, 9.5],
-    [9.6, 9.7, 10.6, 10.65, 10.75, 10.9, 10,8, 10.45, 9.8, 9.5, 9.5, 9.5],
-    [9.7, 9.8, 10.7, 10.75, 10.85, 11.00, 10,9, 10.55, 9.9, 9.6, 9.6, 9.6],
-    [9.7, 9.8, 10.7, 10.75, 10.85, 11.00, 10,9, 10.55, 9.9, 9.6, 9.6, 9.6],
-    [9.3, 9.4, 10.2, 10.25, 10.35, 10.45, 10,4, 10.4, 9.6, 9.3, 9.3, 9.3],
+    [9.4, 9.5, 10.4, 10.5, 10.6, 10.75, 10.65, 10.25, 9.5, 9.2, 9.2, 9.2],
+    [9.4, 9.5, 10.4, 10.5, 10.6, 10.75, 10.65, 10.25, 9.6, 9.3, 9.3, 9.3],
+    [9.5, 9.6, 10.5, 10.6, 10.7, 10.85, 10.75, 10.35, 9.7, 9.4, 9.4, 9.4],
+    [9.6, 9.7, 10.6, 10.65, 10.75, 10.9, 10.8, 10.45, 9.8, 9.5, 9.5, 9.5],
+    [9.6, 9.7, 10.6, 10.65, 10.75, 10.9, 10.8, 10.45, 9.8, 9.5, 9.5, 9.5],
+    [9.7, 9.8, 10.7, 10.75, 10.85, 11.00, 10.9, 10.55, 9.9, 9.6, 9.6, 9.6],
+    [9.7, 9.8, 10.7, 10.75, 10.85, 11.00, 10.9, 10.55, 9.9, 9.6, 9.6, 9.6],
+    [9.3, 9.4, 10.2, 10.25, 10.35, 10.45, 10.4, 10.4, 9.6, 9.3, 9.3, 9.3],
 ];
 let rangoDias = [30, 60, 90, 120, 150, 180, 240, 360, 540, 720, 1080, 1440, 1799];
 let rangoMontos = [500, 5000, 20000, 50000, 200000, 500000, 1000000, 5000000];
@@ -36,11 +37,10 @@ function buscarTasa (dias, monto){
 
 function buscarMejorTasa(monto){
     tasasrespectivasMonto = tablaTasas.at(buscarFilaTasas(monto));
+    mejorTasa = Math.max(...tasasrespectivasMonto);
+    diasTasaMasAlta = tasasrespectivasMonto.indexOf(mejorTasa);
+    console.log(`La mejor tasa es de ${mejorTasa} entre ${rangoDias[diasTasaMasAlta]} y ${rangoDias[diasTasaMasAlta+1]-1} días`);
 }
-
-
-asignarTexto('#title-cdt','Simulador Rendimientos CDT');
-asignarTexto('#text-description-cdt', 'Por favor ingrese el valor y los meses a los cuales quiere abrir tu CDT');
 
 function mensajesAlCliente(){
     montoDeInversion =parseInt(prompt('Por favor ingrese el valor que quiere invertir (minimo 500)'));
@@ -51,8 +51,16 @@ function mensajesAlCliente(){
     }    
 }
 
+
+
+
+
+asignarTexto('#title-cdt','Simulador Rendimientos CDT');
+asignarTexto('#text-description-cdt', 'Por favor ingrese el valor y los meses a los cuales quiere abrir tu CDT');
+
 mensajesAlCliente();
 interesEfectivoAnual = buscarTasa(tiempoDeInversion, montoDeInversion);
 console.log(interesEfectivoAnual);
+buscarMejorTasa(montoDeInversion);
 
 
